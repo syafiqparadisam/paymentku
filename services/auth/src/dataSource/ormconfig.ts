3869ec4b4cdf806352cc23f0e8cb500b20ac87cd
syafiqparadisam <syafiqparadisam@gmail.com>
2024-06-14 11:13:05 +0700

    test

--- services/auth/src/dataSource/ormconfig.ts
+++ services/auth/src/dataSource/ormconfig.ts
@@ -1,20 +1,20 @@
-const { DataSource } = require('typeorm')
-const { MysqlConnectionOptions } = require('typeorm/driver/mysql/MysqlConnectionOptions')
-const dotenv  = require('dotenv')
+import { DataSource } from 'typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import dotenv from 'dotenv';
 dotenv.config();
 
-const mysqlOptionProd: typeof MysqlConnectionOptions = {
-    type: 'mysql',
-    entities: ['./dist/**/**/*.entity.{ts,js}'],
-    host: process.env.DB_HOST,
-    poolSize: 10,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWD,
-    database: process.env.DB_NAME,
-    logging: true,
-    synchronize: false,
-    migrations: ['**/migration/*.ts'],
+const mysqlOptionProd: MysqlConnectionOptions = {
+  type: 'mysql',
+  entities: ['./dist/**/**/*.entity.{ts,js}'],
+  host: process.env.DB_HOST,
+  poolSize: 10,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWD,
+  database: process.env.DB_NAME,
+  logging: true,
+  synchronize: false,
+  migrations: ['**/migration/*.ts'],
 };
 
-module.exports = new DataSource(mysqlOptionProd);
+export default new DataSource(mysqlOptionProd);
